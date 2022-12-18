a571e88f825d7f2c1e7d4e979e0d8bcb311dccad
ANIEBIERE <justinaumoh2@gmail.com>
2022-12-18 12:14:29 -0800

    compulsory optional

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,43 +1,34 @@
-#nclude "main.h"
-
+#include "main.h"
 
 /**
- *
- * print_triangle - a function that prints a triangle
- *
- * @size: size of the triangle
- *
- * Return: triangle of '#'s
- *
+ * print_triangle - entry point
+ * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
  */
 
 void print_triangle(int size)
-
 {
+	int row, hashes, spaces;
 
-		int sp, ro, tr;
-
-
-			if (size <= 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
-				_putchar('\n');
+				_putchar(' ');
 			}
-			else
+			for (hashes = 1; hashes <= row; hashes++)
 			{
-
-				for (ro = 0; ro <= (size - 1); ro++)
-				{
-															for (sp = 0; sp < (size - 1) - ro; sp++)
-					{
-																_putchar(' ');
-															}
-																for (tr = 0; tr <= ro; tr++)
-						{
-							_putchar('#');
+				_putchar('#');
 			}
 			_putchar('\n');
-
 		}
 	}
-
 }
