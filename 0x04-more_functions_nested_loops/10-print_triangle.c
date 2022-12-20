58a538625503b1d9e6c2cfa487f18b571cf144bf
ANIEBIERE <justinaumoh2@gmail.com>
2022-12-20 04:51:48 -0800

    mined

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,61 +1,34 @@
-#include"main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_triangle - prints a triangle
- *
- *   *
- *
- *    * @size: size of the triangle
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+ * print_triangle - entry point
+ * owned by Aniebiere Umoh
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
 
 void print_triangle(int size)
-
 {
+	int row, hashes, spaces;
 
-		int hght, base;
-
-
-
-			if (size <= 0)
-
-						_putchar('\n');
-
-				else
-
-						{
-
-									for (hght = 1; hght <= size; ++hght)
-
-												{
-
-																for (base = 1; base <= size; ++base)
-
-																				{
-
-																									if ((hght + base) <= size)
-
-																															_putchar(' ');
-
-																													else
-
-																																			_putchar('#');
-
-																																}
-
-																			_putchar('\n');
-
-																					}
-
-										}
-
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
