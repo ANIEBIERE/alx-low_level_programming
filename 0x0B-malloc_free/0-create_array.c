0830a54f13cd74d3ba39784493636871faf210ac
ANIEBIERE <justinaumoh2@gmail.com>
2023-01-09 05:49:18 -0800

    changed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,27 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * create_array - creates an array of chars and
- * initialises it with a specific char
- * @size: size of the array
- * @c: the character that initialises the array
- * Return: a pointer to the array, NULL on failure
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
  */
 char *create_array(unsigned int size, char c)
 {
+	char *cr;
 	unsigned int i;
-
-	char *array = malloc(sizeof(char) * size);
 
 	if (size == 0)
 		return (NULL);
 
-	if (array == NULL)
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
 		return (NULL);
-	/*Allocating char c to all array slots*/
+
 	for (i = 0; i < size; i++)
-		array[i] = c;
+		cr[i] = c;
 
-	return (array);
-
-}/**Prince solomon*/
+	return (cr);
+}
